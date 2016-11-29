7ba3e0bc4dc33a711189e7b8d1d955fc6e410199
Sven <s@rouvinez.me>
2016-11-29 10:56:03 +0100

    first commit

---
+++ labo07/Labo_07_Meyer_Rouvinez.cpp
@@ -0,0 +1,29 @@
+/*
+---------------------------------------------------------------------------------------
+Laboratoire : Labo07
+
+Fichier     : Labo07_Meyer_Rouvinez.cpp
+
+Auteur(s)   : 
+
+Date        : Nov 29, 2016
+
+But         : 
+
+Remarque(s) : 
+
+Compilateur : gcc version 6.2.1 20160916 (Red Hat 6.2.1-2) (GCC)
+---------------------------------------------------------------------------------------
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+	return EXIT_SUCCESS;
+}
+
