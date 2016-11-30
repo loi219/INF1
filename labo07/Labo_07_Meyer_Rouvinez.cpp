2d703ecd343cdc46a413f11ee450ecfea5a471df
yohann.meyer <yohann.meyer@gmail.com>
2016-11-30 10:32:42 +0100

    Function genLake(), norme(), area()

--- labo07/Labo_07_Meyer_Rouvinez.cpp
+++ labo07/Labo_07_Meyer_Rouvinez.cpp
@@ -21,8 +21,11 @@ Compilateur : gcc version 6.2.1 20160916 (Red Hat 6.2.1-2) (GCC)
 #include <iomanip>
 #include <climits>
 #include <string>
+#include <cmath>
 
 #define CLEAR_BUFFER cin.ignore(INT_MAX, '\n')
+#define WIDTH_INT_BASE 4u
+#define WIDTH_TEXT_BASE 30u
 
 
 using namespace std;
@@ -63,7 +66,7 @@ bool doAgain();
  * @return:
  *      userInput : int contianing the value that the user entered
  */
-int input(string message, int limitMin, int limitMax, const int WIDTH_INT=WIDTH_INT_BASE, const int WIDTH_TEXT=WIDTH_TEXT_BASE, const string error="Mauvaise saisie. Veuillez reessayez");
+int input(const string& message, int limitMin, int limitMax, const int WIDTH_INT=WIDTH_INT_BASE, const int WIDTH_TEXT=WIDTH_TEXT_BASE, const string error="Mauvaise saisie. Veuillez reessayez");
 /*
  * Goal: Show a prompt for the user to definitely quit the program.
  *
@@ -77,26 +80,44 @@ void toQuit(string message);
 
 
 int main(){
+    srand(time(NULL));
+
+
+    int nbExplo,
+    mapX,
+    mapY,
+    posExploX = 0,
+    posExploY = 0,
+    posTreasureX,
+    posTreasureY,
+    posLake1X,
+    posLake1Y,
+    radLake1,
+    posLake2X,
+    posLake2Y,
+    radLake2,
+    posLake3X,
+    posLake3Y,
+    radLake3;
+
+
+const int   NB_EXPLO_MIN = 1,
+            NB_EXPLO_MAX = INT_MAX,
+            MAP_X_MIN = 1,
+            MAP_Y_MIN = 1,
+            MAP_Y_MAX = INT_MAX,
+            MAP_X_MAX = INT_MAX,
+            LAKE_R_MIN = 1,
+            LAKE_R_MAX = 50;
 
 
 
+    cout << "Ce programme calcule la probabilité pour N chercheurs de trouver un trésor sur une carte de taille X Y." << endl << endl;
 
+    nbExplo = input("Entrez le nombre d'explorateurs :",NB_EXPLO_MIN, NB_EXPLO_MAX );
+    mapX    = input("Entrez la largeur de la carte :",MAP_X_MIN, MAP_X_MAX );
+    mapY    = input("Entrez la hauteur de la carte :",MAP_Y_MIN, MAP_Y_MAX );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 
 
 
@@ -106,6 +127,81 @@ int main(){
 }
 
 
+int area(int mapX, int mapY){
+    static int area;
+    if(!area)
+        area = mapX*mapY;
+    return area;
+}
+int genRandomVal(const int minVal, const int maxVal){
+
+    /*TODO CHANGE RESPONSE TO SOMETHING MORE CIVILISED THAN KILL*/
+    if(min>=max){
+        return EXIT_FAILURE;
+    }
+    int range;
+    range = maxVal-minVal;
+    return rand()%((range*2 +1)-range);
+
+}
+int genRadius(int& r, const int minR, const int maxR){
+    /*TMP*/
+}
+int norme(int x, int y){
+    return (int)sqrt(x*x+y*y);
+}
+int genLake(int& posLake1X, int& posLake1Y, int& radLake1, int& posLake2X, int& posLake2Y, int& radLake2, int& posLake3X, int& posLake3Y, int& radLake3, const int LAKE_R_MIN, const int LAKE_R_MAX, const int minX, const int minY, const int maxX, const int maxY){
+    /*TODO CHANGE OR FOR AND*/
+    posLake1X = genRandomVal(minX, maxX);
+    posLake1Y = genRandomVal(minY, maxY);
+    do{
+    posLake2X = genRandomVal(minX, maxX);
+    posLake2Y = genRandomVal(minY, maxY);
+    }while(posLake2X == posLake1X || posLake2Y == posLake1Y);
+    do{
+    posLake3X = genRandomVal(minX, maxX);
+    posLake3Y = genRandomVal(minY, maxY);
+    }while(posLake3X == posLake1X || posLake3Y == posLake1Y || posLake3X == posLake2X || posLake3Y == posLake2Y);
+
+
+    radLake1  = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
+    do{
+    radLake2  = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
+    }while(radLake2+radLake1>=norme(posLake2X-posLake1X, posLake2Y-posLake1Y));
+    do {
+        radLake3 = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
+    }while(radLake3+radLake1>=norme(posLake3X-posLake1X, posLake3Y-posLake1Y) || radLake3+radLake2>=norme(posLake3X-posLake2X, posLake3Y-posLake2Y));
+
+    
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***************************SAISIE****************************/
 bool doAgain(){
 
     char const YES = 'Y';
