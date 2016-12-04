7aeefcedcaf51119c035d403115d430cb4ea6d55
Sven <s@rouvinez.me>
2016-12-04 18:01:05 +0100

    start shiftExplo

--- labo07/Labo_07_Meyer_Rouvinez.cpp
+++ labo07/Labo_07_Meyer_Rouvinez.cpp
@@ -1,21 +1,20 @@
 /*
----------------------------------------------------------------------------------------
-Laboratoire : Labo07
+ ---------------------------------------------------------------------------------------
+ Laboratoire : Labo07
 
-Fichier     : Labo07_Meyer_Rouvinez.cpp
+ Fichier     : Labo07_Meyer_Rouvinez.cpp
 
-Auteur(s)   : 
+ Auteur(s)   :
 
-Date        : Nov 29, 2016
+ Date        : Nov 29, 2016
 
-But         : 
+ But         :
 
-Remarque(s) : 
+ Remarque(s) :
 
-Compilateur : gcc version 6.2.1 20160916 (Red Hat 6.2.1-2) (GCC)
----------------------------------------------------------------------------------------
-*/
-
+ Compilateur : gcc version 6.2.1 20160916 (Red Hat 6.2.1-2) (GCC)
+ ---------------------------------------------------------------------------------------
+ */
 
 #include <iostream>
 #include <iomanip>
@@ -27,9 +26,7 @@ Compilateur : gcc version 6.2.1 20160916 (Red Hat 6.2.1-2) (GCC)
 #define WIDTH_INT_BASE 4u
 #define WIDTH_TEXT_BASE 30u
 
-
 using namespace std;
-
 
 /*
  * Goal: asks the user if he wants to restart or exit the program
@@ -66,7 +63,10 @@ bool doAgain();
  * @return:
  *      userInput : int contianing the value that the user entered
  */
-int input(const string& message, int limitMin, int limitMax, const int WIDTH_INT=WIDTH_INT_BASE, const int WIDTH_TEXT=WIDTH_TEXT_BASE, const string error="Mauvaise saisie. Veuillez reessayez");
+int input(const string& message, int limitMin, int limitMax,
+		const int WIDTH_INT = WIDTH_INT_BASE, const int WIDTH_TEXT =
+				WIDTH_TEXT_BASE, const string error =
+				"Mauvaise saisie. Veuillez reessayez");
 /*
  * Goal: Show a prompt for the user to definitely quit the program.
  *
@@ -78,171 +78,149 @@ int input(const string& message, int limitMin, int limitMax, const int WIDTH_INT
  */
 void toQuit(string message);
 
+/**
+ * Goal: calculate the coordinates of the explorators
+ *
+ * @param lastX
+ * @param lastY
+ * @param
+ *
+ * @return position
+ * */
+void shiftExplo(int& posExploX, int& posExploY);
 
-int main(){
-    srand(time(NULL));
+int main() {
+	srand(time(NULL));
 
+	int nbExplo, mapX, mapY, posExploX = 0, posExploY = 0, posTreasureX,
+			posTreasureY, posLake1X, posLake1Y, radLake1, posLake2X, posLake2Y,
+			radLake2, posLake3X, posLake3Y, radLake3;
 
-    int nbExplo,
-    mapX,
-    mapY,
-    posExploX = 0,
-    posExploY = 0,
-    posTreasureX,
-    posTreasureY,
-    posLake1X,
-    posLake1Y,
-    radLake1,
-    posLake2X,
-    posLake2Y,
-    radLake2,
-    posLake3X,
-    posLake3Y,
-    radLake3;
+	const int NB_EXPLO_MIN = 1, NB_EXPLO_MAX = INT_MAX, MAP_X_MIN = 1,
+			MAP_Y_MIN = 1, MAP_Y_MAX = INT_MAX, MAP_X_MAX = INT_MAX,
+			LAKE_R_MIN = 1, LAKE_R_MAX = 50;
 
+	cout
+			<< "Ce programme calcule la probabilité pour N chercheurs de trouver un trésor sur une carte de taille X Y."
+			<< endl << endl;
 
-const int   NB_EXPLO_MIN = 1,
-            NB_EXPLO_MAX = INT_MAX,
-            MAP_X_MIN = 1,
-            MAP_Y_MIN = 1,
-            MAP_Y_MAX = INT_MAX,
-            MAP_X_MAX = INT_MAX,
-            LAKE_R_MIN = 1,
-            LAKE_R_MAX = 50;
-
-
-
-    cout << "Ce programme calcule la probabilité pour N chercheurs de trouver un trésor sur une carte de taille X Y." << endl << endl;
-
-    nbExplo = input("Entrez le nombre d'explorateurs :",NB_EXPLO_MIN, NB_EXPLO_MAX );
-    mapX    = input("Entrez la largeur de la carte :",MAP_X_MIN, MAP_X_MAX );
-    mapY    = input("Entrez la hauteur de la carte :",MAP_Y_MIN, MAP_Y_MAX );
-
-
-
-
-
-    doAgain();
-    return EXIT_SUCCESS;
+	//nbExplo = input("Entrez le nombre d'explorateurs :",NB_EXPLO_MIN, NB_EXPLO_MAX);
+	//mapX    = input("Entrez la largeur de la carte :",MAP_X_MIN, MAP_X_MAX );
+	//mapY    = input("Entrez la hauteur de la carte :",MAP_Y_MIN, MAP_Y_MAX );
+shiftExplo(posExploX, posExploY);
+	doAgain();
+	return EXIT_SUCCESS;
 }
 
-
-int area(int mapX, int mapY){
-    static int area;
-    if(!area)
-        area = mapX*mapY;
-    return area;
+int area(int mapX, int mapY) {
+	static int area;
+	if (!area)
+		area = mapX * mapY;
+	return area;
 }
-int genRandomVal(const int minVal, const int maxVal){
+int genRandomVal(const int minVal, const int maxVal) {
 
-    /*TODO CHANGE RESPONSE TO SOMETHING MORE CIVILISED THAN KILL*/
-    if(min>=max){
-        return EXIT_FAILURE;
-    }
-    int range;
-    range = maxVal-minVal;
-    return rand()%((range*2 +1)-range);
+	/*TODO CHANGE RESPONSE TO SOMETHING MORE CIVILISED THAN KILL*/
+	if (minVal >= maxVal) {
+		return EXIT_FAILURE;
+	}
+	int range;
+	range = maxVal - minVal;
+	return rand() % ((range * 2 + 1) - range);
 
 }
-int genRadius(int& r, const int minR, const int maxR){
-    /*TMP*/
+int genRadius(int& r, const int minR, const int maxR) {
+	/*TMP*/
 }
-int norme(int x, int y){
-    return (int)sqrt(x*x+y*y);
+int norme(int x, int y) {
+	return (int) sqrt(x * x + y * y);
 }
-int genLake(int& posLake1X, int& posLake1Y, int& radLake1, int& posLake2X, int& posLake2Y, int& radLake2, int& posLake3X, int& posLake3Y, int& radLake3, const int LAKE_R_MIN, const int LAKE_R_MAX, const int minX, const int minY, const int maxX, const int maxY){
-    /*TODO CHANGE OR FOR AND*/
-    posLake1X = genRandomVal(minX, maxX);
-    posLake1Y = genRandomVal(minY, maxY);
-    do{
-    posLake2X = genRandomVal(minX, maxX);
-    posLake2Y = genRandomVal(minY, maxY);
-    }while(posLake2X == posLake1X || posLake2Y == posLake1Y);
-    do{
-    posLake3X = genRandomVal(minX, maxX);
-    posLake3Y = genRandomVal(minY, maxY);
-    }while(posLake3X == posLake1X || posLake3Y == posLake1Y || posLake3X == posLake2X || posLake3Y == posLake2Y);
 
+void shiftExplo(int& posExploX, int& posExploY) {
 
-    radLake1  = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
-    do{
-    radLake2  = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
-    }while(radLake2+radLake1>=norme(posLake2X-posLake1X, posLake2Y-posLake1Y));
-    do {
-        radLake3 = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
-    }while(radLake3+radLake1>=norme(posLake3X-posLake1X, posLake3Y-posLake1Y) || radLake3+radLake2>=norme(posLake3X-posLake2X, posLake3Y-posLake2Y));
+//enum class Cardi {N=1,S=-1,W=-1,E=1};
 
-    
+	posExploX = genRandomVal(-1, 1);
+	posExploY = genRandomVal(-1, 1);
 
-
-
-
-
+	cout << posExploX << " " << posExploY;
 
 }
 
+int genLake(int& posLake1X, int& posLake1Y, int& radLake1, int& posLake2X,
+		int& posLake2Y, int& radLake2, int& posLake3X, int& posLake3Y,
+		int& radLake3, const int LAKE_R_MIN, const int LAKE_R_MAX,
+		const int minX, const int minY, const int maxX, const int maxY) {
+	// TODO CHANGE OR FOR AND
+	posLake1X = genRandomVal(minX, maxX);
+	posLake1Y = genRandomVal(minY, maxY);
+	do {
+		posLake2X = genRandomVal(minX, maxX);
+		posLake2Y = genRandomVal(minY, maxY);
+	} while (posLake2X == posLake1X || posLake2Y == posLake1Y);
+	do {
+		posLake3X = genRandomVal(minX, maxX);
+		posLake3Y = genRandomVal(minY, maxY);
+	} while (posLake3X == posLake1X || posLake3Y == posLake1Y
+			|| posLake3X == posLake2X || posLake3Y == posLake2Y);
 
+	radLake1 = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
+	do {
+		radLake2 = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
+	} while (radLake2 + radLake1
+			>= norme(posLake2X - posLake1X, posLake2Y - posLake1Y));
+	do {
+		radLake3 = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
+	} while (radLake3 + radLake1
+			>= norme(posLake3X - posLake1X, posLake3Y - posLake1Y)
+			|| radLake3 + radLake2
+					>= norme(posLake3X - posLake2X, posLake3Y - posLake2Y));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 /***************************SAISIE****************************/
-bool doAgain(){
+bool doAgain() {
 
-    char const YES = 'Y';
-    char const NO = 'N';
-    char answer;
-    bool isValid;
+	char const YES = 'Y';
+	char const NO = 'N';
+	char answer;
+	bool isValid;
 
+	do {
+		cout << "Voulez-vous recommencer ? [" << YES << "/" << NO << "]";
+		isValid = bool(cin >> answer);
+		if (isValid)
+			cin.clear();
+		CLEAR_BUFFER;
+		// works even if the user forget the capital letter
+	} while (!(isValid || toupper(answer) == YES || toupper(answer) == NO));
 
-    do{
-        cout << "Voulez-vous recommencer ? ["<<YES<<"/"<<NO<<"]";
-        isValid = bool(cin >> answer);
-        if(isValid)
-            cin.clear();
-        CLEAR_BUFFER;
-        // works even if the user forget the capital letter
-    }while(!(isValid || toupper(answer) == YES || toupper(answer) == NO));
-
-    return(toupper(answer)==YES);
+	return (toupper(answer) == YES);
 }
 
+int input(const string message, int limitMin, int limitMax, const int WIDTH_INT,
+		const int WIDTH_TEXT, const string error) {
+	int userInput;
+	bool isValid;
+	do {
+		//shows message that includes the boundaries that the user must respect
+		cout << message << setw(WIDTH_TEXT - message.length()) << ": [ "
+				<< setw(WIDTH_INT) << limitMin << " et " << setw(WIDTH_INT)
+				<< limitMax << "] :";
+		isValid = bool(cin >> userInput);
 
-int input(const string message, int limitMin, int limitMax,const int WIDTH_INT, const int WIDTH_TEXT, const string error){
-    int userInput;
-    bool isValid;
-    do{
-        //shows message that includes the boundaries that the user must respect
-        cout  << message << setw(WIDTH_TEXT-message.length()) << ": [ " << setw(WIDTH_INT) << limitMin << " et " << setw(WIDTH_INT) << limitMax <<"] :" ;
-        isValid = bool(cin >> userInput);
+		if (!isValid || userInput < limitMin || userInput > limitMax) {
+			cout << error << endl;
+			cin.clear();
+		}
+		CLEAR_BUFFER;
 
-        if(!isValid || userInput < limitMin || userInput > limitMax ){
-            cout << error << endl;
-            cin.clear();
-        }
-        CLEAR_BUFFER;
+	} while (!isValid || userInput < limitMin || userInput > limitMax);
 
-    }while(!isValid || userInput < limitMin || userInput > limitMax);
-
-    return userInput;
+	return userInput;
 }
 
-void toQuit(string message){
-    cout << message;
-    cin.get();
+void toQuit(string message) {
+	cout << message;
+	cin.get();
 }
