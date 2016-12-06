a37a8afd6cc54c6d54ad1451236016bb295ff9fe
yohann.meyer <yohann.meyer@gmail.com>
2016-12-06 01:29:42 +0100

    Version w/ test before split in files

--- labo07/Labo_07_Meyer_Rouvinez.cpp
+++ labo07/Labo_07_Meyer_Rouvinez.cpp
@@ -26,6 +26,7 @@ Compilateur : gcc version 6.2.1 20160916 (Red Hat 6.2.1-2) (GCC)
 #define CLEAR_BUFFER cin.ignore(INT_MAX, '\n')
 #define WIDTH_INT_BASE 4u
 #define WIDTH_TEXT_BASE 30u
+
 enum class State {RICH,LOST,DROWND,SPENT,WALK};
 
 using namespace std;
@@ -72,7 +73,8 @@ int spawn(int& posExploX, int& posExploY, int& posTreasureX, int& posTreasureY, 
 int move(int& posExploX, int& posExploY);
 State checkPos(int& posExplo, int& posTreasure, int& posLake1, int& radLake1, int& posLake2, int& radLake2, int& posLake3, int& radLake3, const int& min, const int& max);
 
-string printPos(State intState);
+void printState(State intState, int explo, int step);
+int defineState(State stateX, State stateY, State& finalState);
 
 
 
@@ -117,14 +119,15 @@ int main() {
             MAP_X_MAX = 100,
             MAP_Y_MAX = 200,
             LAKE_R_MIN = 0,
-            LAKE_R_MAX = min(MAP_X_MAX,MAP_Y_MAX)/2;
+            LAKE_R_MAX =  min(MAP_X_MAX,MAP_Y_MAX)/2;
 
 
     do {
         cout << "Ce programme calcule la probabilité pour N chercheurs de trouver un trésor sur une carte de taille X Y."
              << endl << endl;
 
-        nbExplo = input("Entrez le nombre d'explorateurs :", NB_EXPLO_MIN, NB_EXPLO_MAX);
+        nbExplo = input("Entrez le nombre d'explorateurs", NB_EXPLO_MIN, NB_EXPLO_MAX);
+
     /*    mapX    = input("Entrez la largeur de la carte :",MAP_X_MIN, MAP_X_MAX );
         mapY    = input("Entrez la hauteur de la carte :",MAP_Y_MIN, MAP_Y_MAX ); */
 
@@ -142,37 +145,35 @@ int main() {
                   posLake1X,  posLake1Y,  radLake1,
                   posLake2X,  posLake2Y,  radLake2,
                   posLake3X,  posLake3Y,  radLake3,
-                  LAKE_R_MIN, LAKE_R_MAX, MAP_X_MIN,  MAP_Y_MIN, MAP_X_MAX, MAP_Y_MAX);
-            cout << "IT HAS SPAWNED " << explo << endl;
+                  LAKE_R_MIN, LAKE_R_MAX,
+                  MAP_X_MIN,  MAP_Y_MIN, MAP_X_MAX, MAP_Y_MAX);
             int step;
             for (step=0; step<MAP_X_MAX*MAP_Y_MAX;++step){
 
                 move(posExploX,posExploY);
+                
                 stateX = checkPos(posExploX,  posTreasureX,  posLake1X,  radLake1,  posLake2X,  radLake2,  posLake3X,  radLake3, MAP_X_MIN, MAP_X_MAX);
                 stateY = checkPos(posExploY,  posTreasureY,  posLake1Y,  radLake1,  posLake2Y,  radLake2,  posLake3Y,  radLake3, MAP_Y_MIN, MAP_Y_MAX);
-                if(stateX == State::RICH && stateY == State::RICH){
-                                 finalState = State::RICH;
-                 break;
-                }else if(stateX == State::LOST || stateY == State::LOST){
-					finalState = State::LOST;
-                break;
-                }else if(stateX == State::DROWND && stateY == State::DROWND){
-					finalState = State::DROWND;
-                }else{
-                	finalState = State::WALK;
-                }
+                if(defineState(stateX, stateY, finalState))
+                    break;
             }
-
-
-            cout << "finalState : "<< printPos(finalState) << endl;
-                        foundTreasure += finalState == State::RICH ? 1 : 0;
-                       avgSteps += finalState == State::RICH ? step : 0;
+            
+            printState(finalState, explo, step);
+            cout << posExploX << " : "  << posExploY << endl;
+            foundTreasure += finalState == State::RICH ? 1 : 0;
+            avgSteps += finalState == State::RICH ? step : 0;
 
 
         }
+        
         cout << "Nombre de chercheurs fructueux : " << foundTreasure << endl
              << "Probabilité de succès : " << 100.*foundTreasure/nbExplo << "%" << endl
              << "Nombre de pas moyens au succès : " << (!foundTreasure ? 0 : avgSteps/foundTreasure) << endl;
+        cout << "lac 1 " << posLake1X << "-" << posLake1Y <<" : " << radLake1 << endl;
+        cout << "lac 2 " << posLake2X << "-" << posLake2Y <<" : " << radLake2 << endl;
+        cout << "lac 3 " << posLake3X << "-" << posLake3Y <<" : " << radLake3 << endl;
+        cout << "Tresor" << posTreasureX << "." << posTreasureY << endl;
+
 
 
 
@@ -206,8 +207,8 @@ int norme(int x, int y){
 }
 int spawn(int& posExploX, int& posExploY, int& posTreasureX, int& posTreasureY, int& posLake1X, int& posLake1Y, int& radLake1, int& posLake2X, int& posLake2Y, int& radLake2, int& posLake3X, int& posLake3Y, int& radLake3, const int LAKE_R_MIN, const int LAKE_R_MAX, const int minX, const int minY, const int maxX, const int maxY){
     /*TODO CHANGE OR FOR AND*/
-    static bool first = true;
-    if(first){
+    static bool first;
+    if(!first){
         posLake1X = genRandomVal(minX, maxX);
         posLake1Y = genRandomVal(minY, maxY);
         radLake1  = genRandomVal(LAKE_R_MIN, LAKE_R_MAX);
@@ -221,7 +222,7 @@ int spawn(int& posExploX, int& posExploY, int& posTreasureX, int& posTreasureY, 
         do{
         posLake3X = genRandomVal(minX, maxX);
         posLake3Y = genRandomVal(minY, maxY);
-        radLake3 = genRandomVal(LAKE_R_MIN, min(norme(posLake3X-posLake2X,posLake3Y-posLake2Y),norme(posLake3X-posLake1X,posLake3Y-posLake1Y))-1);
+        radLake3 = genRandomVal(LAKE_R_MIN, min(min(LAKE_R_MAX, norme(posLake3X-posLake2X,posLake3Y-posLake2Y)),norme(posLake3X-posLake1X,posLake3Y-posLake1Y))-1);
 
         }while(norme(posLake3X-posLake1X,posLake3Y-posLake1Y)<=radLake1 ||
                norme(posLake3X-posLake2X,posLake3Y-posLake2Y)<=radLake2 ||
@@ -244,7 +245,7 @@ int spawn(int& posExploX, int& posExploY, int& posTreasureX, int& posTreasureY, 
             posTreasureY = genRandomVal(minY, maxY);
         }while(norme(posTreasureX-posLake1X, posTreasureY-posLake1Y ) <= radLake1 || norme(posTreasureX-posLake2X, posTreasureY-posLake2Y ) <= radLake2 || norme(posTreasureX-posLake3X, posTreasureY-posLake3Y ) <= radLake3);
     }
-    first = !first;
+    first = true;
     /*EXPLO*/
     do{
         posExploX = genRandomVal(minX, maxX);
@@ -270,18 +271,33 @@ State checkPos(int& posExplo, int& posTreasure, int& posLake1, int& radLake1, in
     /*  1     2     3    -      0 */
 
 	return posExplo < min ? State::LOST : posExplo > max ? State::LOST :
-			posExplo == posTreasure ? State::RICH :
-			posExplo <= posLake1 + radLake1 ? State::DROWND :
-			posExplo <= posLake2 + radLake2 ? State::DROWND :
-			posExplo <= posLake3 + radLake3 ? State::DROWND : State::WALK;
+           posExplo == posTreasure ? State::RICH :
+           (norme(posExplo,posLake1) <= radLake1) ? State::DROWND :
+           (norme(posExplo,posLake2) <= radLake2) ? State::DROWND :
+           (norme(posExplo,posLake3) <= radLake3) ? State::DROWND : State::WALK;
 
 }
-string printPos(State state) {
+int defineState(State stateX, State stateY, State& finalState){
+    if(stateX == State::LOST || stateY == State::LOST){
+        finalState = State::LOST;
+        return 1;
+    }else if(stateX == State::RICH && stateY == State::RICH){
+        finalState = State::RICH;
+        return 1;
+    }else if(stateX == State::DROWND && stateY == State::DROWND){
+        finalState = State::DROWND;
+        return 1;
+    }else{
+        finalState = State::WALK;
+        return 0;
+    }
+}
+void printState(State state, int explo, int step) {
 	string actualState;
-
+    static bool first;
 	switch (state) {
 	case State::RICH:
-		actualState = "Riche";
+		actualState = "Riche!";
 		break;
 	case State::LOST:
 		actualState = "Perdu";
@@ -296,12 +312,15 @@ string printPos(State state) {
 		actualState = "Marche";
 		break;
 	default:
-		actualState = "inconnu";
+		actualState = "en chemin";
 		break;
 
 	}
+    if(!first)
+        cout << "Explorateur" << " | " << setw(WIDTH_INT_BASE) << "Etat"<< " | " << setw(WIDTH_INT_BASE) << " #Pas" << endl;
+    first = true;
+    cout << "Explorateur " << "  "<< setw(WIDTH_INT_BASE)   << actualState << "  "<< setw(WIDTH_INT_BASE)  << step << endl;
 
-	return actualState;
 }
 
 
